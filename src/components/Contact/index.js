f8cdd212d21cb6ef78552b98320f2cfa3f867866
Carlos Eduardo <carlos.edumart@outlook.com>
2020-12-01 14:51:09 -0300

    Animation Image p1

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import "./styles.css"
 import emailjs from 'emailjs-com'
 
 function Contact() {
-  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
   const OnSubmit = (event)  => {
     event.preventDefault()
     emailjs.sendForm('gmail', `${process.env.REACT_APP_TEMPLATE_ID}`, event.target, `${process.env.REACT_APP_USER_ID}`)
@@ -20,14 +24,14 @@ function Contact() {
         <div className="BlockCentral">
           <div className="BlockLeftContact">
               <form onSubmit={(event) => {OnSubmit(event)}} >
-              <input name="name" placeholder="Name" className="InputUnic"></input>
+              <input value={name} onChange= {(event)=> {setName(event.target.value)}} name="name" placeholder="Name" className="InputUnic"></input>
               <div className="DivDoubleInput">
-                  <input name="email" placeholder="Email" className="InputDouble"> 
+                  <input value={email} onChange= {(event)=> {setEmail(event.target.value)}} name="email" placeholder="Email" className="InputDouble"> 
                   </input>
-                  <input name="subject" placeholder="Subject" className="InputDouble">
+                  <input value={subject}  onChange= {(event)=> {setSubject(event.target.value)}} name="subject" placeholder="Subject" className="InputDouble">
                   </input>
               </div>
-              <textarea name="message" placeholder="Your Message"></textarea>  
+              <textarea onChange= {(event)=> {setMessage(event.target.value)}} value={message} name="message" placeholder="Your Message"></textarea>  
               <button type="submit" className="buttonDownload"> Enviar</button>  
               </form>
           </div>
