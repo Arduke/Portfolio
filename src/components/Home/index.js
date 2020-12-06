68061981e81207076f8f0b0d103c73ee7d9c513f
Carlos Eduardo <carlos.edumart@outlook.com>
2020-12-06 17:45:51 -0300

    Pequena Correção

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ import code_thinking from '../../image/code_thinking.svg'
 function Home() {
   return (
     <div id="home" className="Component">
-        <div className="BlockLeft" data-anime="left">
+        <div className="BlockLeft">
             <p className="home">Hello There,</p>
             <div>
             <h1 className="line typing-animation">I Am Carlos Eduardo </h1>
@@ -14,7 +14,7 @@ function Home() {
                 I'm a Web Developer with knowledge in React Native, React.js and Node.js. My specialty at the moment is the creation of professional websites and mobile applications for management.
             </article>
         </div>
-        <div className="BlockRight" data-anime="rigth">
+        <div className="BlockRight">
             <img className="code_thinking" alt="developer" src={code_thinking} ></img>
         </div>
     </div>
